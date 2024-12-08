df3dcfd863f46135be84ece1dceed3026c7206a8
Polina <gulienko.polina@mail.ru>
2024-12-08 21:36:04 +0500

    2

---
+++ Tyuiu.GulienkoPO.Sprint6.Task2.V17/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.GulienkoPO.Sprint6.Task2.V17
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
