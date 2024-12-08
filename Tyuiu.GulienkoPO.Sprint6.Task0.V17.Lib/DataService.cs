d6cc9d51e29eb4854d53547c1a157bccc147ce70
Polina <gulienko.polina@mail.ru>
2024-12-08 17:18:13 +0500

    Add project files.

---
+++ Tyuiu.GulienkoPO.Sprint6.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.GulienkoPO.Sprint6.Task0.V17.Lib
+{
+    public class DataService : ISprint6Task0V17
+    {
+        public double Calculate(int x)
+        {
+
+            double res = Math.Round(Math.Exp(x) / x, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.GulienkoPO.Sprint6.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+
+using Tyuiu.GulienkoPO.Sprint6.Task0.V17.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint6.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void VAlidCalc()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 6.695;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
