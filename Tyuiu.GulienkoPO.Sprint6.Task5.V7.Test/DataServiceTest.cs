23581cc48896c35ba30f618024e1a19b1f0892c6
Polina <gulienko.polina@mail.ru>
2024-12-10 18:38:28 +0500

    5

---
+++ Tyuiu.GulienkoPO.Sprint6.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.GulienkoPO.Sprint6.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
